514acdcdd816add9736b22b6e7e4ab452a36b72b
Vincent Petry <pvince81@owncloud.com>
2013-10-28 20:22:06 +0100

    Files, trashbin, public apps use ajax/JSON for the file list
    Files app:
    
    - removed file list template, now rendering list from JSON response
    - FileList.addFile/addDir is now FileList.add() and takes a JS map with all required
      arguments instead of having a long number of function arguments
    - added unit tests for many FileList operations
    - fixed newfile.php, newfolder.php and rename.php to return the file's
      full JSON on success
    - removed obsolete/unused undo code
    - removed download_url / loading options, now using
      Files.getDownloadUrl() for that
    - server side now uses Helper::getFileInfo() to prepare file JSON response
    - previews are now client-side only
    
    Breadcrumbs are now JS only:
    
    - Added BreadCrumb class to handle breadcrumb rendering and events
    - Added unit test for BreadCrumb class
    - Moved all relevant JS functions to the BreadCrumb class
    
    Public page now uses ajax to load the file list:
    
    - Added Helper class in sharing app to make it easier to authenticate
      and retrieve the file's real path
    - Added ajax/list.php to retrieve the file list
    - Fixed FileActions and FileList to work with the ajax list
    
    Core:
    
    - Fixed file picker dialog to use the same list format as files app

--- apps/files/tests/js/fileactionsSpec.js
+++ apps/files/tests/js/fileactionsSpec.js
@@ -22,6 +22,7 @@
 /* global OC, FileActions, FileList */
 describe('FileActions tests', function() {
 	var $filesTable;
+
 	beforeEach(function() {
 		// init horrible parameters
 		var $body = $('body');
@@ -34,23 +35,35 @@ describe('FileActions tests', function() {
 		$('#dir, #permissions, #filestable').remove();
 	});
 	it('calling display() sets file actions', function() {
-		// note: download_url is actually the link target, not the actual download URL...
-		var $tr = FileList.addFile('testName.txt', 1234, new Date(), false, false, {download_url: 'test/download/url'});
+		var fileData = {
+			id: 18,
+			type: 'file',
+			name: 'testName.txt',
+			mimetype: 'plain/text',
+			size: '1234',
+			etag: 'a01234c',
+			mtime: '123456'
+		};
 
-		// no actions before call
-		expect($tr.find('.action[data-action=Download]').length).toEqual(0);
-		expect($tr.find('.action[data-action=Rename]').length).toEqual(0);
-		expect($tr.find('.action.delete').length).toEqual(0);
+		// note: FileActions.display() is called implicitly
+		var $tr = FileList.add(fileData);
 
-		FileActions.display($tr.find('td.filename'), true);
-
-		// actions defined after cal
+		// actions defined after call
 		expect($tr.find('.action[data-action=Download]').length).toEqual(1);
 		expect($tr.find('.nametext .action[data-action=Rename]').length).toEqual(1);
 		expect($tr.find('.action.delete').length).toEqual(1);
 	});
 	it('calling display() twice correctly replaces file actions', function() {
-		var $tr = FileList.addFile('testName.txt', 1234, new Date(), false, false, {download_url: 'test/download/url'});
+		var fileData = {
+			id: 18,
+			type: 'file',
+			name: 'testName.txt',
+			mimetype: 'plain/text',
+			size: '1234',
+			etag: 'a01234c',
+			mtime: '123456'
+		};
+		var $tr = FileList.add(fileData);
 
 		FileActions.display($tr.find('td.filename'), true);
 		FileActions.display($tr.find('td.filename'), true);
@@ -62,14 +75,22 @@ describe('FileActions tests', function() {
 	});
 	it('redirects to download URL when clicking download', function() {
 		var redirectStub = sinon.stub(OC, 'redirect');
-		// note: download_url is actually the link target, not the actual download URL...
-		var $tr = FileList.addFile('test download File.txt', 1234, new Date(), false, false, {download_url: 'test/download/url'});
+		var fileData = {
+			id: 18,
+			type: 'file',
+			name: 'testName.txt',
+			mimetype: 'plain/text',
+			size: '1234',
+			etag: 'a01234c',
+			mtime: '123456'
+		};
+		var $tr = FileList.add(fileData);
 		FileActions.display($tr.find('td.filename'), true);
 
 		$tr.find('.action[data-action=Download]').click();
 
 		expect(redirectStub.calledOnce).toEqual(true);
-		expect(redirectStub.getCall(0).args[0]).toEqual(OC.webroot + '/index.php/apps/files/ajax/download.php?dir=%2Fsubdir&files=test%20download%20File.txt');
+		expect(redirectStub.getCall(0).args[0]).toEqual(OC.webroot + '/index.php/apps/files/ajax/download.php?dir=%2Fsubdir&files=testName.txt');
 		redirectStub.restore();
 	});
 });

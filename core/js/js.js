2398b11ac53678dac7d9a69bc62f6c89cd810444
Vincent Petry <pvince81@owncloud.com>
2014-01-28 16:16:09 +0100

    Fixed parseQueryString to handle empty values and plus signs
    - now correctly parse query strings with '+' signs
    - empty values are now parsed either as null or empty string
    - added unit test for parseQueryString()

--- core/js/js.js
+++ core/js/js.js
@@ -347,6 +347,7 @@ var OC={
 	 */
 	parseQueryString:function(queryString){
 		var parts,
+			pos,
 			components,
 			result = {},
 			key,
@@ -354,12 +355,25 @@ var OC={
 		if (!queryString){
 			return null;
 		}
-		if (queryString[0] === '?'){
-			queryString = queryString.substr(1);
+		pos = queryString.indexOf('?');
+		if (pos >= 0){
+			queryString = queryString.substr(pos + 1);
 		}
-		parts = queryString.split('&');
+		parts = queryString.replace(/\+/g, '%20').split('&');
 		for (var i = 0; i < parts.length; i++){
-			components = parts[i].split('=');
+			// split on first equal sign
+			var part = parts[i]
+			pos = part.indexOf('=');
+			if (pos >= 0) {
+				components = [
+					part.substr(0, pos),
+					part.substr(pos + 1)
+				]
+			}
+			else {
+				// key only
+				components = [part];
+			}
 			if (!components.length){
 				continue;
 			}
@@ -367,8 +381,14 @@ var OC={
 			if (!key){
 				continue;
 			}
-			value = components[1];
-			result[key] = value && decodeURIComponent(value);
+			// if equal sign was there, return string
+			if (components.length > 1) {
+				result[key] = decodeURIComponent(components[1]);
+			}
+			// no equal sign => null value
+			else {
+				result[key] = null;
+			}
 		}
 		return result;
 	},
